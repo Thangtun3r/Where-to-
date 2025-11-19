136121c0c6e4e2e13501f927296b79fea51c6162
DumDino <tqhieu10072004@gmail.com>
2025-11-19 15:14:21 +0700

    CamSwappingDone

--- Assets/Scripts/DrivingPOVManager.cs
+++ Assets/Scripts/DrivingPOVManager.cs
@@ -1,54 +1,56 @@
 using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DrivingPOVManager : MonoBehaviour
 {
-    public CinemachineVirtualCamera camFront;
-    public CinemachineVirtualCamera camLeft;
-    public CinemachineVirtualCamera camRight;
-    public CinemachineVirtualCamera camBack;
+    public List<CinemachineVirtualCamera> POVCams;
 
-    [Range(0.0f, 0.3f)]
-    public float edgeThickness = 0.05f;
-
+    [Range(0.0f, 0.1f)]
+    public float edgeThickness = 0.02f;
     private int screenW;
-    private int screenH;
+
+    [SerializeField] private int currentCam = 2;
+
+    public CinemachineBrain cineBrain;
 
     void Start()
     {
+        //Determine where screen edges would be on set resolution
         screenW = Screen.width;
-        screenH = Screen.height;
     }
 
     void Update()
     {
         Vector3 m = Input.mousePosition;
 
-        bool leftEdge = m.x < screenW * edgeThickness;
-        bool rightEdge = m.x > screenW * (1 - edgeThickness);
-        bool topEdge = m.y > screenH * (1 - edgeThickness);
-        bool bottomEdge = m.y < screenH * edgeThickness;
+        bool rightEdge = m.x < screenW * edgeThickness;
+        bool leftEdge = m.x > screenW * (1 - edgeThickness);
 
-        // horizontal edges
-        if (leftEdge) SwitchTo(camLeft);
-        else if (rightEdge) SwitchTo(camRight);
-
-        // vertical edges (optional)
-        else if (topEdge) SwitchTo(camFront);
-        else if (bottomEdge) SwitchTo(camBack);
+        // mouse touch edge -> update currentCam and use it as an idex to switch to correspoding object in POVCams list
+        if (leftEdge && currentCam < POVCams.Count - 1 && !cineBrain.IsBlending)
+        {
+            currentCam++;
+            SwitchToCurrentCam();
+        }
+        else if (rightEdge && currentCam > 0 && !cineBrain.IsBlending)
+        {
+            currentCam--;
+            SwitchToCurrentCam();
+        }
     }
 
-    void SwitchTo(CinemachineVirtualCamera cam)
+    private void SwitchToCurrentCam()
     {
-        // Lower all priorities
-        camFront.Priority = 5;
-        camLeft.Priority = 5;
-        camRight.Priority = 5;
-        camBack.Priority = 5;
+        // Lower all cam prio
+        foreach (var p in POVCams)
+        {
+            p.Priority = 5;
+        }
 
-        // Boost this one
-        cam.Priority = 10;
+        // Boost currentCam prio
+        POVCams[currentCam].Priority = 10;
     }
 }
