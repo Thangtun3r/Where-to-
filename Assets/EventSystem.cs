7b4735a069b62aa4a2a6e9b6429e03ea66993f0a
DumDino <tqhieu10072004@gmail.com>
2025-12-19 21:04:49 +0700

    AWEAFGSDHM

---
+++ Assets/EventSystem.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EventSystem : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+
+    }
+}
