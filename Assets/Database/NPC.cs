2fcb1b1eeef8d58aec3be7fa6fe612f615084e9a
DumDino <tqhieu10072004@gmail.com>
2026-01-31 17:11:12 +0700

    huzayyyy

---
+++ Assets/Database/NPC.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewNPC", menuName = "ScriptableObject/NPC")]
+public class NPC: ScriptableObject
+{
+    public string npcId;
+    public string npcName;
+    public string preference;
+}
---
+++ Assets/Editor/ImportNPCData.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class NewBehaviourScript
+{
+    private static string npcDatabasePath = "/Database/NPCSpawnSchedule.csv";
+    [MenuItem ("Tools/ImportNPCData")]
+    public static void ImportNPCData()
+    {
+        string[] allLines = File.ReadAllLines(Application.dataPath + npcDatabasePath);
+        foreach (string line in allLines)
+        {
+            if(line == allLines[0])
+                continue;
+
+            string[] npcData = line.Split(",");
+            
+            NPC npc = ScriptableObject.CreateInstance<NPC>();
+            npc.npcId = npcData[0];
+            npc.npcName = npcData[1];
+            npc.preference = npcData[2];
+
+            AssetDatabase.CreateAsset(npc, "Assets/Database/NPCs/" + npc.npcId + ".asset");
+        }
+        AssetDatabase.SaveAssets();
+    }
+}
